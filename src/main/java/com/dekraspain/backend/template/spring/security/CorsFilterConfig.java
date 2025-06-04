3ee5bc7efd71c6b9e429d147e38f294025dc1dc6
Antonio Alvarez Lopez <antonio.alvarez@dekra.com>
2025-06-04 11:02:10 +0200

    Update email templates and build commands for compliance notifications

--- src/main/java/com/dekraspain/backend/template/spring/security/CorsFilterConfig.java
+++ src/main/java/com/dekraspain/backend/template/spring/security/CorsFilterConfig.java
@@ -21,6 +21,7 @@ public class CorsFilterConfig {
         "https://dome-certification.dome-marketplace-dev2.org",
         "https://dome-certification.dome-marketplace-prd.org",
         "https://dome-certification.dome-marketplace.org",
+        "https://dome-certification.dome-marketplace.eu",
         "http://localhost:4200"
       )
     );
